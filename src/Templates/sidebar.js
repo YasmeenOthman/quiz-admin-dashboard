07addc656037fb2283f95966d05ffdbc63689cca
YasmeenOthman <yasmeen.othman20@gmail.com>
2024-08-27 13:50:00 +0300

    sidebar icons

--- src/Templates/sidebar.js
+++ src/Templates/sidebar.js
@@ -11,28 +11,47 @@ import {
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import QuizIcon from "@mui/icons-material/Quiz";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import PeopleIcon from "@mui/icons-material/People";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import HandymanIcon from "@mui/icons-material/Handyman";
 import StatsIcon from "@mui/icons-material/BarChart";
 import LogoutIcon from "@mui/icons-material/Logout";
+import AddIcon from "@mui/icons-material/Add";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
 import { Link } from "react-router-dom";
 
 const list = [
   {
     title: "Quizzez",
     icon: <QuizIcon />,
-    items: ["Manage", "Create New Quiz", "All Quizzez"],
+    items: [
+      { name: "Manage", path: "/", icon: <HandymanIcon /> },
+      { name: "Create New Quiz", path: "/create-quiz", icon: <AddIcon /> },
+      {
+        name: "All Quizzez",
+        path: "/quizzes",
+        icon: <FormatListBulletedIcon />,
+      },
+    ],
   },
   {
     title: "Users",
     icon: <PeopleIcon />,
-    items: ["View All", "Progress"],
+    items: [
+      {
+        name: "View All",
+        path: "/users/view-all",
+        icon: <ManageAccountsIcon />,
+      },
+      { name: "Progress", path: "/users/progress", icon: <QueryStatsIcon /> },
+    ],
   },
-  { title: "Statistics", icon: <StatsIcon /> },
-  { title: "Logout", icon: <LogoutIcon /> },
+  { title: "Statistics", icon: <StatsIcon />, path: "/statistics" },
+  { title: "Logout", icon: <LogoutIcon />, path: "/logout" },
 ];
 
 const Sidebar = () => {
-  // State to manage which sections are open
   const [openSections, setOpenSections] = useState({
     Quizzez: false,
     Users: false,
@@ -60,7 +79,6 @@ const Sidebar = () => {
         <List>
           {list.map((item, index) => {
             if (item.items) {
-              // Render collapsible items with sub-items
               const { title, icon, items } = item;
               const open = openSections[title] || false;
               return (
@@ -76,10 +94,12 @@ const Sidebar = () => {
                         <ListItem
                           button
                           component={Link}
-                          to={`/${subItem.toLowerCase().replace(/\s+/g, "-")}`}
+                          to={subItem.path}
                           key={subIndex}
+                          sx={{ pl: 4 }} // Add left padding to indent sub-items
                         >
-                          <ListItemText inset primary={subItem} />
+                          <ListItemIcon>{subItem.icon}</ListItemIcon>
+                          <ListItemText primary={subItem.name} />
                         </ListItem>
                       ))}
                     </List>
@@ -87,9 +107,8 @@ const Sidebar = () => {
                 </React.Fragment>
               );
             } else {
-              // Render simple list items with icons
               return (
-                <ListItem button key={index}>
+                <ListItem button key={index} component={Link} to={item.path}>
                   <ListItemIcon>{item.icon}</ListItemIcon>
                   <ListItemText primary={item.title} />
                 </ListItem>
